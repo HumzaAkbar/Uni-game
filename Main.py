9d893457a0034710aa33013f82f00a3be8f32ae3
HumzaAkbar <humza.waheed.akbar786@gmail.com>
2023-10-17 23:54:56 +0100

    Movement left test

--- Main.py
+++ Main.py
@@ -7,7 +7,8 @@ pygame.display.set_caption("Humza and Adam Game")
 
 Colour = (255,0,225)
 
-FPS = 30
+FPS = 60
+VEL = 5
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(
@@ -40,7 +41,10 @@ def main():
                 run = False
         
         
-        red.x += 1
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_a]: # Left
+            yellow.x -= VEL 
+
         draw_window(red, yellow)
     
     pygame.quit()
