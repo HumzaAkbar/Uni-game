5562dbe26743060226bb476e086df85e61a2ed05
HumzaAkbar <humza.waheed.akbar786@gmail.com>
2023-10-17 23:46:04 +0100

    Player 1 movement test

--- Main.py
+++ Main.py
@@ -20,14 +20,17 @@ RED_SPACESHIP_IMAGE = pygame.image.load(
 RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
     RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
-def draw_window():
+def draw_window(red, yellow):
     WIN.fill(Colour)
-    WIN.blit(YELLOW_SPACESHIP, (300, 100))
-    WIN.blit(RED_SPACESHIP, (700, 100))
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+    WIN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()
 
 
 def main():
+    red = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    yellow = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -37,7 +40,8 @@ def main():
                 run = False
         
         
-        draw_window()
+        red.x += 1
+        draw_window(red, yellow)
     
     pygame.quit()
 
