aad9e637a9dd1f9a7babbfc2cc3e05b69a05d4ff
HumzaAkbar <humza.waheed.akbar786@gmail.com>
2023-11-01 20:56:38 +0000

    Sound issue removed sound game runs

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import pygame
 import os
 pygame.font.init()
-pygame.mixer.init
+
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -14,15 +14,15 @@ YELLOW = (255, 255, 0)
 
 BORDER = pygame.Rect(WIDTH//2 - 5, 0, 10, HEIGHT)
 
-BULLET_HIT_SOUND = pygame.mixer.Sound(os.path.join('Assets/Grenade+1.mp3'))
-BULLET_FIRE_SOUND = pygame.mixer.Sound(os.path.join('Assets/Gun+Silencer.mp3'))
+
+
 
 HEALTH_FONT = pygame.font.SysFont('arial', 40)
 WINNER_FONT = pygame.font.SysFont('arial', 100)
 
 FPS = 60
 VEL = 5
-BULLET_VEL = 7 
+BULLET_VEL = 10
 MAX_BULLETS = 3 
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
@@ -132,23 +132,24 @@ def main():
 
             if event.type == pygame.KEYDOWN: 
                if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
-                   bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height//2 -2, 10, 5)
+                   bullet = pygame.Rect(
+                       yellow.x + yellow.width, yellow.y + yellow.height//2 -2, 10, 5)
                    yellow_bullets.append(bullet)
-                   BULLET_FIRE_SOUND.play()
+                   
 
                    if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
                        bullet =pygame.Rect(
                            red.x, red.y + red.height//2 - 2, 10,5)
                        red_bullets.append(bullet)
-                       BULLET_FIRE_SOUND.play()
+                       
     
             if event.type == RED_HIT:
                 red_health -= 1
-                BULLET_HIT_SOUND.play()
+                
 
             if event.type == YELLOW_HIT: 
                 yellow_health -= 1
-                BULLET_HIT_SOUND.play()
+                
 
         winner_text = ""
         if red_health <= 0:
@@ -161,7 +162,7 @@ def main():
             draw_winner(winner_text)
             break
 
-
+        
         keys_pressed = pygame.key.get_pressed() 
         yellow_handle_movement(keys_pressed, yellow)
         red_handle_movement(keys_pressed, red)
